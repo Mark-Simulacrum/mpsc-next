d7a070071bb53332e91428c3a10240174e026f74
Mark Rousskov <mark.simulacrum@gmail.com>
2019-08-13 20:33:18 -0400

    Replace spinlock with Mutex
    Though this damages performance significantly, it's more realistic from
    a final implementation perspective as spinlocks are quite
    hard/impossible to do well in userspace.

--- src/queue.rs
+++ src/queue.rs
@@ -1,67 +1,39 @@
-use std::cell::UnsafeCell;
 use std::collections::VecDeque;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Mutex;
 
 #[derive(Debug)]
 pub struct Queue<T> {
     bounded: Option<usize>,
-    lock: AtomicBool,
-    v: UnsafeCell<VecDeque<T>>,
+    v: Mutex<VecDeque<T>>,
 }
-
-unsafe impl<T: Send> Send for Queue<T> {}
-unsafe impl<T: Sync> Sync for Queue<T> {}
 
 impl<T> Queue<T> {
     pub fn unbounded() -> Queue<T> {
         Queue {
             bounded: None,
-            lock: AtomicBool::new(false),
-            v: UnsafeCell::new(VecDeque::new()),
+            v: Mutex::new(VecDeque::new()),
         }
     }
 
     pub fn bounded(capacity: usize) -> Queue<T> {
         Queue {
             bounded: Some(capacity),
-            lock: AtomicBool::new(false),
-            v: UnsafeCell::new(VecDeque::with_capacity(capacity)),
+            v: Mutex::new(VecDeque::with_capacity(capacity)),
         }
-    }
-
-    fn acquire_lock(&self) {
-        while self.lock.compare_and_swap(false, true, Ordering::SeqCst) {
-            // busy loop
-        }
-    }
-
-    unsafe fn release_lock(&self) {
-        // We don't swap here because it's guaranteed that we're the ones that acquired the lock
-        // per the CAS above
-        self.lock.store(false, Ordering::SeqCst);
     }
 
     pub fn push(&self, value: T) -> Result<(), T> {
-        unsafe {
-            self.acquire_lock();
-            let buf = &mut *self.v.get();
-            if let Some(max_buf) = self.bounded {
-                if buf.len() >= max_buf {
-                    return Err(value);
-                }
+        let mut buf = self.v.lock().unwrap();
+        if let Some(max_buf) = self.bounded {
+            if buf.len() >= max_buf {
+                return Err(value);
             }
-            buf.push_back(value);
-            self.release_lock();
         }
+        buf.push_back(value);
         Ok(())
     }
 
     pub fn pop(&self) -> Option<T> {
-        unsafe {
-            self.acquire_lock();
-            let res = (&mut *self.v.get()).pop_front();
-            self.release_lock();
-            res
-        }
+        self.v.lock().unwrap().pop_front()
     }
 }
